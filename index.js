a126561d7e3b24c5a89d66649ef56dc8a8d64cb4
ManoharMahanthi <87300351+ManoharMahanthi@users.noreply.github.com>
2021-08-28 12:41:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+
+const Discord = require('discord.js');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const intents = new Discord.Intents(32767);
+
+const client = new Discord.Client({ intents });
+
+client.on('ready', () => {
+	console.log('ready!');
+});
+
+client.on('messageCreate', msg => {
+	// console.log(msg.content);
+	if (msg.content == 'hello') {
+		msg.reply('hola');
+	}
+	else if (msg.content.includes('kingu')) {
+		msg.reply('bonguuu');
+	}
+});
+
+client.login(process.env.TOKEN);
