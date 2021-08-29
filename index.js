cf088872027c205c870f552b6bf0f0ca8735cf94
DileepKothapalli <65917448+DileepKothapalli@users.noreply.github.com>
2021-08-29 09:55:10 +0530

    added a comment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//  I added a comment
+
 /* eslint-disable no-unused-vars */
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -10,18 +12,17 @@ const intents = new Discord.Intents(32767);
 
 const client = new Discord.Client({ intents });
 
-client.on('ready', () => {
-	console.log('ready!');
+client.on("ready", () => {
+  console.log("ready!");
 });
 
-client.on('messageCreate', msg => {
-	// console.log(msg.content);
-	if (msg.content == 'hello') {
-		msg.reply('hola');
-	}
-	else if (msg.content.includes('kingu')) {
-		msg.reply('bonguuu');
-	}
+client.on("messageCreate", (msg) => {
+  // console.log(msg.content);
+  if (msg.content == "hello") {
+    msg.reply("hola");
+  } else if (msg.content.includes("kingu")) {
+    msg.reply("bonguuu");
+  }
 });
 
 client.login(process.env.TOKEN);
