bc4d9305b0d64ff4bad6b255912e2e27ac175543
ManoharMahanthi <manoharmahanthi008@gmail.com>
2021-08-30 16:48:44 +0530

    url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("messageCreate", (msg) => {
 
     var config = {
       method: "get",
-      url: "https://api.twitter.com/2/tweets/1275828087666679809?tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld",
+      url: "https://zenquotes.io/api/random",
       headers: {},
     };
 
